70ae2f5e769d0d33ac6619810a01381acd704852
Tomasz Kozysa <tomek@15nine.com>
2020-01-24 12:28:04 +0000

    ga set

--- plugins/ga.js
+++ plugins/ga.js
@@ -15,7 +15,7 @@ export default ({ app }) => {
     /*
     ** Set the current page
     */
-    ga('create', 'UA-XXXXXXXX-X', 'auto')
+    ga('create', 'UA-41924883-2', 'auto')
     /*
     ** Every time the route changes (fired on initialization too)
     */
