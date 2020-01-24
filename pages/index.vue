8fc1d0961690d1625bef3bb8f99dd97052420085
Tomasz Kozysa <tomek@15nine.com>
2020-01-24 19:26:42 +0000

    typo!

--- pages/index.vue
+++ pages/index.vue
@@ -49,7 +49,7 @@
 
     <div class="about-the-project">
       <p>Rainbow Spinner, is a side project that explores SVG as a self contained animation format. </p>
-      <p>SMIL, animation markup for SVG allows easy access to animation parameters for customisation, while a CSS variable is an easy copy&amp;paste way to add a spinner to your project. What you get is an animted SVG, wrapped in a CSS variable. You can also download the animated SVG file.</p>      
+      <p>SMIL, animation markup for SVG allows easy access to animation parameters for customisation, while a CSS variable is an easy copy&amp;paste way to add a spinner to your project. What you get is an animated SVG, wrapped in a CSS variable. You can also download the animated SVG file.</p>      
       
       <p class="atp-sidenote">This website has been built with vue.js &amp; nuxt.js, 
         source code at <a href="https://github.com/tomekkozysa/svg-spinner">@github</a></p>
